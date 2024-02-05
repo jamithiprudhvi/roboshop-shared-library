975ed954c0405aea27d4477ce0ea617cb8f74c19
prudhvi <jamithireddyprudhvi9395@gmail.com>
2024-02-06 01:45:29 +0530

    jenkins file

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -1,117 +1,118 @@
-pipeline {
-    agent {
-        node {
-            label 'AGENT-1'
+def call(Map configMap){
+    pipeline {
+        agent {
+            node {
+                label 'AGENT-1'
+            }
         }
-    }
-    environment { 
-        packageVersion = ''
-        nexusURL = '172.31.2.43:8081'
-    }
-    options {
-        timeout(time: 1, unit: 'HOURS') 
-        disableConcurrentBuilds()
-    }
-    parameters {
-        // string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+        environment { 
+            packageVersion = ''
+            nexusURL = '172.31.5.95:8081'
+        }
+        options {
+            timeout(time: 1, unit: 'HOURS')
+            disableConcurrentBuilds()
+        }
+        parameters {
+            // string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
 
-        // text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')
+            // text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')
 
-        booleanParam(name: 'Deploy', defaultValue: false, description: 'Toggle this value')
+            booleanParam(name: 'Deploy', defaultValue: false, description: 'Toggle this value')
 
-        // choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
+            // choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
 
-        // password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
-    }
-    // Build
-    stages {
-        stage('Get the version') {
-            steps {
-                script{
-                    def packageJson = readJSON file: 'package.json'
-                    packageVersion = packageJson.version
-                    echo "application version: $packageVersion"
-
+            // password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
+        }
+        // build
+        stages {
+            stage('Get the version') {
+                steps {
+                    script {
+                        def packageJson = readJSON file: 'package.json'
+                        packageVersion = packageJson.version
+                        echo "application version: $packageVersion"
+                    }
                 }
             }
-        }
-        stage('Install dependencies') {
-            steps {
-                sh """
-                    npm install
-                """
+            stage('Install dependencies') {
+                steps {
+                    sh """
+                        npm install
+                    """
+                }
             }
-        }
-        stage('unit test'){
-            steps{
-                sh """
-                    echo "unit tests will run here"
-                """
-            }            
-        }
-        stage('sonar scan'){
-            steps{
-                sh """
-                    sonar-scanner
-                """
+            stage('Unit tests') {
+                steps {
+                    sh """
+                        echo "unit tests will run here"
+                    """
+                }
             }
-        }
-        stage('Build') {
-            steps {
-                sh """
-                    ls -la
-                    zip -q -r catalogue.zip ./* -x ".git" -x "*.zip"
-                    ls -ltr
-                """
+            stage('Sonar Scan'){
+                steps{
+                    sh """
+                        sonar-scanner
+                    """
+                }
             }
-        }
-        stage('Publish Artificate') {
-            steps {
-                nexusArtifactUploader(
-                    nexusVersion: 'nexus3',
-                    protocol: 'http',
-                    nexusUrl: "${nexusURL}",
-                    groupId: 'com.roboshop',
-                    version: "${packageVersion}",
-                    repository: 'catalogue',
-                    credentialsId: 'nexus-auth',
-                    artifacts: [
-                        [artifactId: 'catalogue',
-                        classifier: '',
-                        file: 'catalogue.zip',
-                        type: 'zip']
-                    ]
-                )
+            stage('Build') {
+                steps {
+                    sh """
+                        ls -la
+                        zip -q -r catalogue.zip ./* -x ".git" -x "*.zip"
+                        ls -ltr
+                    """
+                }
             }
-        }
-        stage('Deploy') {
-            when {
-                expression{
-                    params.Deploy == 'true'
-                }                
-            }
-            steps {
-                script {
-                        def params = [
-                            string(name: 'version', value: "$packageVersion"),
-                            string(name: 'environment', value: "dev")
+            stage('Publish Artifact') {
+                steps {
+                    nexusArtifactUploader(
+                        nexusVersion: 'nexus3',
+                        protocol: 'http',
+                        nexusUrl: "${nexusURL}",
+                        groupId: 'com.roboshop',
+                        version: "${packageVersion}",
+                        repository: 'catalogue',
+                        credentialsId: 'nexus-auth',
+                        artifacts: [
+                            [artifactId: 'catalogue',
+                            classifier: '',
+                            file: 'catalogue.zip',
+                            type: 'zip']
                         ]
-                        build job: "catalogue-deploy", wait: true, parameters: params
+                    )
+                }
+            }
+            stage('Deploy') {
+                when {
+                    expression{
+                        params.Deploy == 'true'
+                    }
+                }
+                steps {
+                    script {
+                            def params = [
+                                string(name: 'version', value: "$packageVersion"),
+                                string(name: 'environment', value: "dev")
+                            ]
+                            build job: "catalogue-deploy", wait: true, parameters: params
+                        }
                 }
             }
         }
-    }
-    // post Build
-    post { 
-        always { 
-            echo 'I will always say Hello again'
-            deleteDir()
-        }
-        failure { 
-            echo 'This runs when pipeline is failed, used generally to send some alerts'
-        }
-        success { 
-            echo 'It will say hello when pipe line is success'
+        // post build
+        post { 
+            always { 
+                echo 'I will always say Hello again!'
+                deleteDir()
+            }
+            failure { 
+                echo 'this runs when pipeline is failed, used generally to send some alerts'
+            }
+            success{
+                echo 'I will say Hello when pipeline is success'
+            }
         }
     }
 }
